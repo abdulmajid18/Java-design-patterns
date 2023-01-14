b5805fe6280339a183d9c23f08c60e3ae896d2d2
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-01-14 09:42:30 +0000

    button interface

---
+++ creational/factory/factory_1/buttons/Button.java
@@ -0,0 +1,11 @@
+package Design_Patterns.creational.factory.factory_1.buttons;
+
+/**
+ * Common interface for all buttoms
+ */
+
+public interface Button {
+    void render();
+
+    void onClick();
+}
