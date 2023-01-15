c8406a53e69c61497c8db9095be353fcd66fd860
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-01-15 22:36:18 +0000

    update

---
+++ creational/singleton/non_thread_safe/DemoSingleThread.java
@@ -0,0 +1,34 @@
+package Design_Patterns.creational.singleton.non_thread_safe;
+
+public class DemoSingleThread {
+    public static void main(String[] args) {
+        System.out.println("If you see the same value, then singleton was resused Else not used");
+
+        Singleton singleton = Singleton.getInstance("FOO");
+        Singleton anotherSingleton = Singleton.getInstance("BAR");
+
+        System.out.println(singleton.value);
+        System.out.println(anotherSingleton.value);
+    }
+}
+
+class Singleton {
+    private static Singleton instance;
+    public String value;
+
+    private Singleton(String value) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value) {
+        if (instance == null) {
+            instance = new Singleton(value);
+        }
+        return instance;
+    }
+}
