23f77f793a8b4f46ada5adc60b4d154ed644db4a
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-01-15 22:32:43 +0000

    dialog

---
+++ creational/factory/factory_1/factory/Dialog.java
@@ -0,0 +1,23 @@
+package Design_Patterns.creational.factory.factory_1.factory;
+
+import Design_Patterns.creational.factory.factory_1.buttons.Button;
+
+/**
+ * Base factory class. Note that "factory" is merely a role for the class. It
+ * should have some core business logic which needs different products to be
+ * created.
+ */
+public abstract class Dialog {
+    public void renderWindow() {
+        // Other code
+        Button okButton = createButton();
+        okButton.render();
+    }
+
+    /**
+     * Subclasses will override this method in order to create specific button
+     * objects.
+     */
+    public abstract Button createButton();
+
+}
