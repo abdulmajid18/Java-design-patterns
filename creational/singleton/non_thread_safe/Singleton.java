7ba49fd1598c84d7e402037ccd248b978133bc67
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-01-15 22:36:23 +0000

    update

---
+++ creational/singleton/non_thread_safe/Singleton.java
@@ -0,0 +1,22 @@
+package Design_Patterns.creational.singleton.non_thread_safe;
+
+public final class Singleton {
+    private static Singleton instance;
+    public String value;
+
+    private Singleton(String value) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value) {
+        if (instance == null) {
+            instance = new Singleton(value);
+        }
+        return instance;
+    }
+}
