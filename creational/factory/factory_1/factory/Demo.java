5d86b5e1cf2cf08a9519c27c092f24d5d4a13e1b
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-01-15 22:32:26 +0000

    demo

---
+++ creational/factory/factory_1/factory/Demo.java
@@ -0,0 +1,32 @@
+package Design_Patterns.creational.factory.factory_1.factory;
+
+public class Demo {
+    private static Dialog dialog;
+
+    public static void main(String[] args) {
+        configure();
+        runBusinessLogic();
+    }
+
+    /**
+     * The concrete factory is usually chosen depending on configuration or
+     * environment options.
+     */
+    static void configure() {
+        if (System.getProperty("os.name").equals("Windows 10")) {
+            dialog = new WindowsDialog();
+        } else {
+            dialog = new HtmlDialog();
+            // dialog = new WindowsDialog();
+        }
+    }
+
+    /**
+     * All of the client code should work with factories and products through
+     * abstract interfaces. This way it does not care which factory it works
+     * with and what kind of product it returns.
+     */
+    static void runBusinessLogic() {
+        dialog.renderWindow();
+    }
+}
