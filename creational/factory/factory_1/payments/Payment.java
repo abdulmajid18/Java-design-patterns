e140126d8441be44fa42e3dd9e06a107c3000e46
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-04-02 16:32:45 +0000

    Product Interface

---
+++ creational/factory/factory_1/payments/Payment.java
@@ -0,0 +1,7 @@
+/* So we have our Product interface
+ * An interface common to our products.
+ * AN methods our Factory or Creator will require to create Products
+ */
+public interface Payment {
+    void pay();
+}
